1fcd290aa1314cada3cd38bb2732e0e634ab8445
Kiên Nguyễn Trung <kientrungng.2002@gmail.com>
2024-05-16 15:20:05 +0700

    mhehe

---
+++ src/Componants/Homepages/Homepage.jsx
@@ -0,0 +1,21 @@
+import { Grid } from '@mui/material'
+import React from 'react'
+import Navigation from '../Navigation/Navigation'
+
+const Homepage = () => {
+  return (
+    <Grid container xs={12} className='px-5 lg:px-36 justify-between'>
+      <Grid item xs={0} lg={2.5} className='hidden lg:block w-full relative'>
+          <Navigation/>
+      </Grid>
+      <Grid item xs={0} lg={6} className='hidden lg:block w-full relative'>
+        <p className='text-center'>Middle part</p>
+      </Grid>
+      <Grid item xs={0} lg={3} className='hidden lg:block w-full relative'>
+        <p className='text-center'>Middle part</p>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default Homepage
